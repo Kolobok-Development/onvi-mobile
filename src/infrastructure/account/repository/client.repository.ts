4e7028db450a904299daf013c70fa13a79458004
Your Name <you@example.com>
2025-06-26 15:25:44 +0300

    логирование данных поступающих на вход метода update

--- src/infrastructure/account/repository/client.repository.ts
+++ src/infrastructure/account/repository/client.repository.ts
@@ -1,16 +1,19 @@
 import { IClientRepository } from '../../../domain/account/client/client-repository.abstract';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ClientEntity } from '../entity/client.entity';
 import { Repository } from 'typeorm';
 import { Client } from '../../../domain/account/client/model/client';
 import { ClientMapper } from '../mapper/client.mapper';
+import { Logger } from 'nestjs-pino';
+
 
 @Injectable()
 export class ClientRepository implements IClientRepository {
   constructor(
     @InjectRepository(ClientEntity)
     private readonly clientRepository: Repository<ClientEntity>,
+        @Inject(Logger) private readonly logger: Logger,
   ) {}
 
   async create(client: Client): Promise<Client> {
@@ -76,6 +79,12 @@ export class ClientRepository implements IClientRepository {
   async update(client: Client): Promise<any> {
     const clientEntity = ClientMapper.toClientEntity(client);
     const { clientId, ...updatedData } = clientEntity;
+
+    this.logger.log({
+      message: "Client repo update",
+      clientId: clientId,
+      updatedData: updatedData
+    })
 
     const updatedClient = await this.clientRepository.update(
       {
