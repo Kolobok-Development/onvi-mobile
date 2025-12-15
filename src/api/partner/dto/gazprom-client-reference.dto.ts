f15e7955ed4c77bbe6ed25f9fa02bfbff6d9253e
Nikita Savitskiy <s6475515243@gmail.com>
2025-12-15 12:43:28 +0400

    removed partner controller

--- src/api/partner/dto/gazprom-client-reference.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class GazpromClientReferenceDto {
-  @IsString()
-  @IsNotEmpty({ message: 'reference is required' })
-  reference: string;
-}
--- src/infrastructure/partner/partner.module.ts
+++ src/infrastructure/partner/partner.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GazpromRepositoryProvider } from './gazprom/provider/gazprom-repository.provider';
 import { GazpromRepository } from './gazprom/repository/gazprom.repository';
-import { GazpromUsecase } from '../../application/usecases/partner/gazprom/gazprom.usecase';
-import { PartnerController } from '../../api/partner/partner.controller';
 import { HttpModule } from '@nestjs/axios';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PartnerEntity } from './entity/partner.entity';
 import { PartnerClientEntity } from './entity/partner-client.entity';
 import { PartnerRepositoryProvider } from './provider/partner-repository.provider';
-import { PartnerUsecase } from '../../application/usecases/partner/partner.usecase';
-import { PartnerStrategy } from '../common/strategies/partner.strategy';
 import { EnvConfigModule } from '../config/env-config/env-config.module';
 
 @Module({
@@ -18,19 +14,15 @@ import { EnvConfigModule } from '../config/env-config/env-config.module';
     TypeOrmModule.forFeature([PartnerEntity, PartnerClientEntity]),
     EnvConfigModule,
   ],
-  controllers: [PartnerController],
+  controllers: [],
   providers: [
     GazpromRepositoryProvider,
     PartnerRepositoryProvider,
-    GazpromUsecase,
     GazpromRepository,
-    PartnerUsecase,
-    PartnerStrategy,
   ],
   exports: [
     GazpromRepositoryProvider,
     PartnerRepositoryProvider,
-    PartnerUsecase,
   ],
 })
 export class PartnerModule {}
