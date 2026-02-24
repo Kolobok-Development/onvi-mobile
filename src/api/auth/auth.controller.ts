f5f753de889cae789850604b689b8cfff0c23bc2
Nikita Savitskiy <s6475515243@gmail.com>
2026-02-24 21:24:02 +0400

    redeploy #2

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -14,6 +14,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 
+
 //import { ThrottlerGuard } from '@nestjs/throttler';
 import { AuthUsecase } from '../../application/usecases/auth/auth.usecase';
 import { LoginRequestDto } from './dto/login-request.dto';
